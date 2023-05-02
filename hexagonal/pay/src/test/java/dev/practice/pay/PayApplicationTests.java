8298f74086198169c882fc17f26116ae0907d12c
starryeye <see2818@naver.com>
2023-05-03 00:21:50 +0900

    [starryeye, 2023.05.03] - pay project init
    내용
    - hexagonal architecture 를 적용한 pay project init

---
+++ hexagonal/pay/src/test/java/dev/practice/pay/PayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
