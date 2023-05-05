b0ae1433c0187dd85def416da7b059583ad7f90e
starryeye <see2818@naver.com>
2023-05-05 22:59:17 +0900

    [starryeye, 2023.05.05] - application port in 개발
    내용
    - 매핑 전략 주석

--- hexagonal/pay/src/main/java/dev/practice/pay/account/application/port/in/GetAccountBalanceQuery.java
+++ hexagonal/pay/src/main/java/dev/practice/pay/account/application/port/in/GetAccountBalanceQuery.java
@@ -3,6 +3,9 @@ package dev.practice.pay.account.application.port.in;
 import dev.practice.pay.account.domain.Account;
 import dev.practice.pay.account.domain.Money;
 
+/**
+ * 양방향 매핑 전략
+ */
 public interface GetAccountBalanceQuery {
 
     Money getAccountBalance(Account.AccountId accountId);
--- hexagonal/pay/src/main/java/dev/practice/pay/account/application/port/in/SendMoneyUseCase.java
+++ hexagonal/pay/src/main/java/dev/practice/pay/account/application/port/in/SendMoneyUseCase.java
@@ -1,5 +1,8 @@
 package dev.practice.pay.account.application.port.in;
 
+/**
+ * 완전 매핑 전략
+ */
 public interface SendMoneyUseCase {
 
     boolean sendMoney(SendMoneyCommand command);
