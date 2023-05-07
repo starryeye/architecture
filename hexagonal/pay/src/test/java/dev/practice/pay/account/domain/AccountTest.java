b4bfca3c5bb2d8c567fc68d0415ef48b33d4508b
starryeye <see2818@naver.com>
2023-05-07 22:45:46 +0900

    [starryeye, 2023.05.07] - Domain Test 코드 작성
    내용
    - withdraw, succes/fail test 코드 작성

--- hexagonal/pay/src/test/java/dev/practice/pay/account/domain/AccountTest.java
+++ hexagonal/pay/src/test/java/dev/practice/pay/account/domain/AccountTest.java
@@ -42,4 +42,76 @@ class AccountTest {
         //then
         Assertions.assertThat(balance).isEqualTo(Money.of(1200L));
     }
+
+    @Test
+    void withdrawSuccess() {
+        //given
+        Account.AccountId accountId = new Account.AccountId(1L);
+        Account.AccountId targetAccountId = new Account.AccountId(101L);
+
+        Activity activity1 = ActivityTestData.defaultActivity()
+                .withTargetAccount(accountId)
+                .withMoney(Money.of(999L))
+                .build();
+        Activity activity2 = ActivityTestData.defaultActivity()
+                .withTargetAccount(accountId)
+                .withMoney(Money.of(1L))
+                .build();
+        Activity activity3 = ActivityTestData.defaultActivity()
+                .withSourceAccount(accountId)
+                .withMoney(Money.of(300L))
+                .build();
+
+        ActivityWindow activityWindow = new ActivityWindow(activity1, activity2, activity3);
+
+        Account account = AccountTestData.defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(activityWindow)
+                .build();
+
+        //when
+        boolean result = account.withdraw(Money.of(1200L), targetAccountId);
+
+        //then
+        Assertions.assertThat(result).isTrue();
+        Assertions.assertThat(account.getActivityWindow().getActivityList()).hasSize(4);
+        Assertions.assertThat(account.calculateBalance()).isEqualTo(Money.of(0L));
+    }
+
+    @Test
+    void withdrawFail() {
+        //given
+        Account.AccountId accountId = new Account.AccountId(1L);
+        Account.AccountId targetAccountId = new Account.AccountId(101L);
+
+        Activity activity1 = ActivityTestData.defaultActivity()
+                .withTargetAccount(accountId)
+                .withMoney(Money.of(999L))
+                .build();
+        Activity activity2 = ActivityTestData.defaultActivity()
+                .withTargetAccount(accountId)
+                .withMoney(Money.of(1L))
+                .build();
+        Activity activity3 = ActivityTestData.defaultActivity()
+                .withSourceAccount(accountId)
+                .withMoney(Money.of(300L))
+                .build();
+
+        ActivityWindow activityWindow = new ActivityWindow(activity1, activity2, activity3);
+
+        Account account = AccountTestData.defaultAccount()
+                .withAccountId(accountId)
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(activityWindow)
+                .build();
+
+        //when
+        boolean result = account.withdraw(Money.of(1201L), targetAccountId);
+
+        //then
+        Assertions.assertThat(result).isFalse();
+        Assertions.assertThat(account.getActivityWindow().getActivityList()).hasSize(3);
+        Assertions.assertThat(account.calculateBalance()).isEqualTo(Money.of(1200L));
+    }
 }
