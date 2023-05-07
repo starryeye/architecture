07400f6f5e6c26405d8010dea2d86644d8d4c56c
starryeye <see2818@naver.com>
2023-05-08 00:36:39 +0900

    [starryeye, 2023.05.08] - Domain ActivityWindow Test 코드 작성
    내용
    - getStartTime, getEndTime

--- hexagonal/pay/src/test/java/dev/practice/pay/account/domain/ActivityWindowTest.java
+++ hexagonal/pay/src/test/java/dev/practice/pay/account/domain/ActivityWindowTest.java
@@ -1,4 +1,62 @@
 package dev.practice.pay.account.domain;
 
+import dev.practice.pay.common.ActivityTestData;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 public class ActivityWindowTest {
+    
+    @Test
+    void getStartTime() {
+        //given
+        LocalDateTime startTime = LocalDateTime.now().minusDays(10);
+        LocalDateTime betweenTime = LocalDateTime.now().minusDays(5);
+        LocalDateTime endTime = LocalDateTime.now();
+
+        Activity startActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(startTime)
+                .build();
+        Activity betweenActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(betweenTime)
+                .build();
+        Activity endActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(endTime)
+                .build();
+        
+        ActivityWindow window = new ActivityWindow(startActivity, betweenActivity, endActivity);
+
+        //when
+        LocalDateTime getStartTime = window.getStartTime();
+
+        //then
+        Assertions.assertThat(getStartTime).isEqualTo(startTime);
+    }
+
+    @Test
+    void getEndTime() {
+        //given
+        LocalDateTime startTime = LocalDateTime.now().minusDays(10);
+        LocalDateTime betweenTime = LocalDateTime.now().minusDays(5);
+        LocalDateTime endTime = LocalDateTime.now();
+
+        Activity startActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(startTime)
+                .build();
+        Activity betweenActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(betweenTime)
+                .build();
+        Activity endActivity = ActivityTestData.defaultActivity()
+                .withCreatedAt(endTime)
+                .build();
+
+        ActivityWindow window = new ActivityWindow(startActivity, betweenActivity, endActivity);
+
+        //when
+        LocalDateTime getEndTime = window.getEndTime();
+
+        //then
+        Assertions.assertThat(getEndTime).isEqualTo(endTime);
+    }
 }
