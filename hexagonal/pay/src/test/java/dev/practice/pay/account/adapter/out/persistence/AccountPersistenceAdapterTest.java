a1a88632d414a37dfa78aef45386a8601a3df848
starryeye <see2818@naver.com>
2023-05-09 22:09:13 +0900

    [starryeye, 2023.05.09] - adapter out persistence Test 코드 작성
    내용
    - saveActivities() Test 코드 작성

--- hexagonal/pay/src/test/java/dev/practice/pay/account/adapter/out/persistence/AccountPersistenceAdapterTest.java
+++ hexagonal/pay/src/test/java/dev/practice/pay/account/adapter/out/persistence/AccountPersistenceAdapterTest.java
@@ -1,7 +1,11 @@
 package dev.practice.pay.account.adapter.out.persistence;
 
 import dev.practice.pay.account.domain.Account;
+import dev.practice.pay.account.domain.ActivityWindow;
 import dev.practice.pay.account.domain.Money;
+import dev.practice.pay.common.AccountTestData;
+import dev.practice.pay.common.ActivityTestData;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,7 +14,6 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest //스프링 컨테이너 + JPA 관련 설정
@@ -20,14 +23,36 @@ class AccountPersistenceAdapterTest {
     @Autowired
     private AccountPersistenceAdapter adapter;
     @Autowired
-    private ActivityRepository repository;
+    private ActivityRepository activityRepository;
 
     @Test
-    @Sql("AccountPersistenceAdapterTest.sql")
+    @Sql("AccountPersistenceAdapterTest.sql") //test data insert
     void loadAccount() {
         Account account = adapter.loadAccount(new Account.AccountId(1L), LocalDateTime.of(2022, 5, 10, 0, 0));
 
-        assertThat(account.getActivityWindow().getActivityList()).hasSize(2);
-        assertThat(account.calculateBalance()).isEqualTo(Money.of(500));
+        Assertions.assertThat(account.getActivityWindow().getActivityList()).hasSize(2);
+        Assertions.assertThat(account.calculateBalance()).isEqualTo(Money.of(500));
+    }
+
+    @Test
+    void saveActivities() {
+        Account account = AccountTestData.defaultAccount()
+                .withBaselineBalance(Money.of(500L))
+                .withActivityWindow(
+                        new ActivityWindow(
+                                ActivityTestData.defaultActivity()
+                                        .withActivityId(null)
+                                        .withMoney(Money.of(1L))
+                                        .build()
+                        )
+                )
+                .build();
+
+        adapter.saveActivities(account);
+
+        Assertions.assertThat(activityRepository.count()).isEqualTo(1);
+
+        ActivityJpaEntity savedActivity = activityRepository.findAll().get(0);
+        Assertions.assertThat(savedActivity.getAmount()).isEqualTo(1L);
     }
 }
