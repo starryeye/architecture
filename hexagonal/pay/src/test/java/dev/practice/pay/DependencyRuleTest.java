b5b7f1057233763eb381b138298268e549952e08
starryeye <see2818@naver.com>
2023-05-11 02:34:32 +0900

    [starryeye, 2023.05.11] - 패키지 의존성 방향 Test
    내용
    - archunit library 사용하여 domain -> application 의존성 Test

--- hexagonal/pay/src/test/java/dev/practice/pay/DependencyRuleTest.java
+++ hexagonal/pay/src/test/java/dev/practice/pay/DependencyRuleTest.java
@@ -1,2 +1,19 @@
-package dev.practice.pay;public class DependencyRuleTest {
+package dev.practice.pay;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+
+public class DependencyRuleTest {
+
+    @Test
+    void testPackageDependencies() {
+        ArchRuleDefinition.noClasses()
+                .that()
+                .resideInAnyPackage("dev.practice.pay.account.domain..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("dev.practice.pay.account.application..")
+                .check(new ClassFileImporter().importPackages("dev.practice.pay.account.."));
+    }
 }
