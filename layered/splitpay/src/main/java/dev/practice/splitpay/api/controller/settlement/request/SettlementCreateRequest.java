0fb335baf55c342cad1ee57ed9acf5c99db800bf
starryeye <see2818@naver.com>
2023-08-30 00:19:45 +0900

    [starryeye, 2023.08.30] - count 필드 추가
    내용
    - TODO

--- layered/splitpay/src/main/java/dev/practice/splitpay/api/controller/settlement/request/SettlementCreateRequest.java
+++ layered/splitpay/src/main/java/dev/practice/splitpay/api/controller/settlement/request/SettlementCreateRequest.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class SettlementCreateRequest {
 
     @Valid
-    @NotEmpty(message = "1/N 정산하기 요청 대상은 필수입니다.")
+//    @NotEmpty(message = "1/N 정산하기 요청 대상은 필수입니다.")
     @Size(min = 2, message = "1/N 정산하기 요청 대상은 2명 이상이어야 합니다.")
     private List<SettlementPieceRequest> settlementPieceRequests;
 
--- layered/splitpay/src/main/java/dev/practice/splitpay/api/service/settlement/SettlementService.java
+++ layered/splitpay/src/main/java/dev/practice/splitpay/api/service/settlement/SettlementService.java
@@ -33,8 +33,9 @@ public class SettlementService {
         SettlementRequest request = settlementRequestRepository.findEntityGraphByRequestId(requestId).orElseThrow();
 
         request.updateDetailsStatus(List.of(receiverId), SettlementDetailStatus.COMPLETED);
+        request.increaseCompletedCount(); //TODO: unit test, 응답 DTO, Optimistic Lock test
 
-        request.checkCompleteStatus();
+        request.checkCompleteStatus(); //TODO : 삭제 고려
 
         return request.getDetailByReceiverId(receiverId);
     }
